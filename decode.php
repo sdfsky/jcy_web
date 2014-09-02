6cb225c67e0f0f21e34e73a15a41abce666d4a86
宋登峰 <sky_php@qq.com>
2014-09-02 18:16:22 +0800

    代码修改

---
+++ decode.php
@@ -0,0 +1,35 @@
+<?php
+	
+	
+$str = 'autostart=false&amp;server=scratch.mit.edu&amp;cloudToken=00000000-0000-0000-0000-000000000000&amp;cdnToken=20cf068e1607e2648364b43f292a470c&amp;urlOverrides=%7B%22sitePrefix%22%3A%22http%3A%2F%2Fscratch.mit.edu%2F%22%2C%22siteCdnPrefix%22%3A%22http%3A%2F%2Fcdn.scratch.mit.edu%2F%22%2C%22assetPrefix%22%3A%22http%3A%2F%2Fassets.scratch.mit.edu%2F%22%2C%22assetCdnPrefix%22%3A%22http%3A%2F%2Fcdn.assets.scratch.mit.edu%2F%22%2C%22projectPrefix%22%3A%22http%3A%2F%2Fprojects.scratch.mit.edu%2F%22%2C%22projectCdnPrefix%22%3A%22http%3A%2F%2Fcdn.projects.scratch.mit.edu%2F%22%2C%22internalAPI%22%3A%22internalapi%2F%22%2C%22siteAPI%22%3A%22site-api%2F%22%2C%22staticFiles%22%3A%22scratchr2%2Fstatic%2F%22%7D&amp;inIE=false&amp;project_id=24606739&amp;project_title=Starfall&amp;project_notes=Thank%20you%20so%20much%2C%20%40xOnic%20for%20Curating%20this!%20%3AD%0A%2BUpdate!%208%2F19%2F14%20Cheating%20is%20now%20impossible.%20%26gt%3B%3AD%0A%2BUpdate!%208%2F19%2F14%20(I%20know%2C%20another%20update)%2C%20but%20I%26%2339%3Bve%20had%20a%20request%20to%20make%20it%20a%20little%20easier%2C%20so%20now%20it%26%2339%3Bs%20a%20bit%20less%20hectic%20later%20on.%20%40deadeye2%20%2B%0A%2BUpdate!%208%2F19%2F14%20There%26%2339%3Bs%20now%20an%20actual%20list%20for%20cheaters%2C%20so%20you%20can%20view%20them%20at%20your%20own%20leisure.%2B%0A*Don%26%2339%3Bt%20cheat%20and%20use%20Turbo%20Mode...%20You%26%2339%3Bve%20been%20warned...*%0A-Be%20sure%20to%20play%20in%20fullscreen%3B%20all%20sprites%20are%20vector-made!%0A-Double%20click%20the%20flag%20any%20time%20you%20restart%2C%20otherwise%2C%20it%20gets%20screwy.%0A-If%20the%20graphics%20are%20messed%20up%20or%20the%20game%20is%20really%20laggy%2C%20pressing%20Ctrl%2BM%20(in%20Windows)%20should%20help%20out%20a%20lot.%0A%0A~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%0A%0AMusic%3A%20%26quot%3BSan%26quot%3B%20by%20longzijun%0A%0ASounds%3A%20Various%20sources%20including%20soundbible.com%2C%20mp3skull.com%2C%20youtube.com%2C%20and%20more.%0A%0AGraphics%3A%20All%20created%20by%20me!%0A&amp;project_credits=Welcome%20to%20Starfall!%20Take%20the%20role%20of%20a%20shooting%20star%20as%20it%20plummets%20towards%20Earth%20while%20dodging%20dangerous%20red%20space%20debris%2C%20and%20bouncing%20off%20harmless%20gray%20debris!%20Use%20the%20powerups%20to%20blast%2C%20burn%2C%20and%20even%20shoot%20your%20way%20down%20to%20the%20blue%20marble%20we%20call%20home.%0A~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%0AHow%20to%20play%3A%0A%3D%3D%3D%3D%3D%3D%3D%3D%3D%0AMovement%0A-Use%20the%20mouse%20to%20guide%20the%20falling%20star%20left%20and%20right%20to%20navigate%20the%20debris%20field%20in%20the%20atmosphere.%0A-------------------%0APowerups%20(Press%20the%20corresponding%20key%20to%20activate)%0A1-%20Jump%3A%20launch%20the%20star%20back%20up%20into%20the%20atmosphere%0A2-%20Stop%3A%20cease%20all%20motion%0A3-%20Invincibility%3A%20temporarily%20become%20invincible%0A4-%20Explosion%3A%20activate%20an%20explosion%20to%20tear%20apart%20any%20debris%20it%20touches%0A5-%20Fireball%3A%20temporarily%20destroy%20any%20debris%20you%20touch%0AMouse%20click-%20Laser%3A%20fire%20a%20laser%20towards%20to%20mouse%20to%20destroy%20debris%0A-----------------------%0AOther%20controls%0A-Space%20bar%3A%20Pause%0A-M%3A%20toggle%20music%0A~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%0AHow%20stuff%20works%3A%0A-Scrolling%3A%20infinite%20scrolling%20in%20all%20directions%20using%20clones%2C%20speed%20variables%2C%20and%20random%20placement%20of%20clones.%0A-Lives%3A%203%20total%20lives.%20Colliding%20with%20red%20debris%20decreases%20the%20lives%20you%20have%20left%20by%20one.%0A-Collisions%3A%20Colliding%20with%20any%20debris%20causes%20you%20to%20bounce%20off%2C%20using%20simplified%20trigonometry%20functions.%0A-Score%3A%20Going%20down%20increases%20score%2C%20going%20up%20decreases%20score.%20Hitting%20gray%20debris%20adds%20100%20points%2C%20while%20hitting%20red%20debris%20takes%20away%20500.%0A-High%20scores%20and%20cheaters%20list%3A%20Everything%20was%20based%20off%20of%20this%20project%20I%20made.%20http%3A%2F%2Fscratch.mit.edu%2Fprojects%2F25191260%2F%0A~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%0AAny%20questions%2C%20comments%2C%20or%20feedback%2C%20please%20drop%20them%20in%20the%20comments%20below!&amp;project_creator=aigkids08&amp;project_modifiedDate=2014-08-20%2002%3A25%3A01&amp;project_isPublished=true';
+$url =  urldecode($str);
+print_r(parse_url($url));
+
+
+$flashvars = array(
+	"autostart"=>"false",
+	"server"=>"scratch.mit.edu",
+	"cloudToken"=>"00000000-0000-0000-0000-000000000000",
+	"cdnToken"=>"20cf068e1607e2648364b43f292a470c",
+	"urlOverrides"=>array(
+            	"sitePrefix"=>"http://scratch.mit.edu/",
+            	"siteCdnPrefix"=>"http://cdn.scratch.mit.edu/",
+            	"assetPrefix"=>"http://assets.scratch.mit.edu/",
+            	"assetCdnPrefix"=>"http://cdn.assets.scratch.mit.edu/",
+            	"projectPrefix"=>"http://projects.scratch.mit.edu/",
+            	"projectCdnPrefix"=>"http://cdn.projects.scratch.mit.edu/",
+            	"internalAPI"=>"internalapi/",
+            	"siteAPI"=>"site-api/",
+            	"staticFiles"=>"scratchr2/static/",
+                "inIE"=>"false",
+                "project_id"=>"24606739",
+                "project_title"=>"Starfall",
+                "project_notes"=>"Starfall",
+                "project_creator"=>"aigkids08",
+                "project_modifiedDate"=>"2014-08-20 02:25:01",
+                "project_isPublished"=>"true",
+
+        )
+);
+
+?>
